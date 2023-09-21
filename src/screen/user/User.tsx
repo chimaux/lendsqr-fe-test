db73f3e4a3a0e5740b6486a829ff0581965bf9f8
Chimaux <ugoppromise@gmail.com>
2023-09-22 02:34:36 +0800

    pagination

--- src/screen/user/User.tsx
+++ src/screen/user/User.tsx
@@ -1,3 +1,6 @@
+import React,{useState}  from "react"
+import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
 import "./user.scss";
 import Layout from "../../layout/Layout";
 import tabIcon1 from "./images/tabIcon.png";
@@ -8,7 +11,7 @@ import filter from './images/filterBtn.png'
 import more from './images/more.png'
 
 
-const usersData = [
+const items = [
   {
     organization:"Lendsqr",
     username:"Adedeji",
@@ -26,12 +29,28 @@ const usersData = [
     status:"pending",
   },
   {
+    organization:"Lendsqr2",
+    username:"Adedeji",
+    email:"adedeji@lendsqr.com",
+    phoneno:"08078903721",
+    datejoined:"May 15, 2020 10:00 AM",
+    status:"active",
+  },
+  {
     organization:"Lendsqr",
     username:"Adedeji",
     email:"adedeji@lendsqr.com",
     phoneno:"08078903721",
     datejoined:"May 15, 2020 10:00 AM",
     status:"pending",
+  },
+  {
+    organization:"Lendsqr3",
+    username:"Adedeji",
+    email:"adedeji@lendsqr.com",
+    phoneno:"08078903721",
+    datejoined:"May 15, 2020 10:00 AM",
+    status:"blacklisted",
   },
   {
     organization:"Lendsqr",
@@ -47,23 +66,7 @@ const usersData = [
     email:"adedeji@lendsqr.com",
     phoneno:"08078903721",
     datejoined:"May 15, 2020 10:00 AM",
-    status:"pending",
-  },
-  {
-    organization:"Lendsqr",
-    username:"Adedeji",
-    email:"adedeji@lendsqr.com",
-    phoneno:"08078903721",
-    datejoined:"May 15, 2020 10:00 AM",
-    status:"pending",
-  },
-  {
-    organization:"Lendsqr",
-    username:"Adedeji",
-    email:"adedeji@lendsqr.com",
-    phoneno:"08078903721",
-    datejoined:"May 15, 2020 10:00 AM",
-    status:"pending",
+    status:"inactive",
   },
   {
     organization:"Lendsqr",
@@ -812,10 +815,6 @@ const usersData = [
 ]
 
 
-
-
-
-
 const tabContent = [
   {
     icon: tabIcon1,
@@ -838,6 +837,103 @@ const tabContent = [
     number: "2,453",
   },
 ];
+// const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+
+function Items({ usersData }: { usersData: any[] }) {
+  return (
+    <>
+<div className="secondContainer">
+<table >
+
+<thead>
+
+<tr className="tr">
+
+
+<th ><div className="thead">ORGANIZATION<img className="icon" src={filter} alt=" " /></div></th>
+<th ><div className="thead">USERNAME<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">EMAIL<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">PHONE NUMBER<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">DATE JOINED<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">STATUS<img className="icon" src={filter} alt=" " /></div></th>
+
+
+</tr>
+
+
+</thead>
+
+
+<tbody>
+
+    {
+       usersData && usersData.map((item,index)=>(
+          <tr key={index.toString()} id={`data${index}`} className="tableData">
+          <td><span className="tdata">{item.organization}</span></td>
+          <td><span className="tdata">{item.username}</span></td>
+          <td><span className="tdata">{item.email}</span></td>
+          <td><span className="tdata">{item.phoneno}</span></td>
+          <td><span className="tdata">i{item.datejoined}</span></td>
+          <td><div className="tdata "><div className={`tdata2 ${item.status === "inactive" ? "inactive": item.status === "active" ? "active" : item.status === "pending" ? "pending" : "blacklist" }`}>{item.status}</div><img className="icon" src={more} alt=" " /></div></td>
+        </tr>
+        ))
+      }
+    </tbody>
+
+
+  </table>
+
+
+</div>
+
+
+    </>
+  );
+}
+
+function PaginatedItems({ itemsPerPage }: { itemsPerPage: number }) {
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const usersData = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event: { selected: number }) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items usersData={usersData} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        activeClassName="active"
+        containerClassName="paginationContainer"
+      />
+    </>
+  );
+}
+
+
+
 
 const User = () => {
   return (
@@ -858,40 +954,60 @@ const User = () => {
       </div>
 {/* HEAD TABS ENDS HERE*/}
 
-<div className="secondContainer">
-<table>
+{/* <div className="secondContainer">
+
+
+
+  
+<table >
+
 <thead>
-<tr>
-    <th ><div className="thead">ORGANIZATION<img className="icon" src={filter} alt=" " /></div></th>
-    <th ><div className="thead">USERNAME<img className="icon" src={filter} alt=" " /></div></th>
-    <th><div className="thead">EMAIL<img className="icon" src={filter} alt=" " /></div></th>
-    <th><div className="thead">PHONE NUMBER<img className="icon" src={filter} alt=" " /></div></th>
-    <th><div className="thead">DATE JOINED<img className="icon" src={filter} alt=" " /></div></th>
-    <th><div className="thead">STATUS<img className="icon" src={filter} alt=" " /></div></th>
-  </tr>
+
+<tr className="tr">
+
+
+<th ><div className="thead">ORGANIZATION<img className="icon" src={filter} alt=" " /></div></th>
+<th ><div className="thead">USERNAME<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">EMAIL<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">PHONE NUMBER<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">DATE JOINED<img className="icon" src={filter} alt=" " /></div></th>
+<th><div className="thead">STATUS<img className="icon" src={filter} alt=" " /></div></th>
+
+
+</tr>
+
+
 </thead>
+
 
 <tbody>
     
-{
-    usersData.map((item,index)=>(
-      <tr key={index.toString()} className="tableData">
-      <td><span className="tdata">{item.organization}</span></td>
-      <td><span className="tdata">{item.username}</span></td>
-      <td><span className="tdata">{item.email}</span></td>
-      <td><span className="tdata">{item.phoneno}</span></td>
-      <td><span className="tdata">i{item.datejoined}</span></td>
-      <td><div className="tdata "><div className="tdata2 pending">{item.status}</div><img className="icon" src={more} alt=" " /></div></td>
-    </tr>
-    ))
-  }
-</tbody>
+    {
+        usersData.map((item,index)=>(
+          <tr key={index.toString()} id={`data${index}`} className="tableData">
+          <td><span className="tdata">{item.organization}</span></td>
+          <td><span className="tdata">{item.username}</span></td>
+          <td><span className="tdata">{item.email}</span></td>
+          <td><span className="tdata">{item.phoneno}</span></td>
+          <td><span className="tdata">i{item.datejoined}</span></td>
+          <td><div className="tdata "><div className={`tdata2 ${item.status === "inactive" ? "inactive": item.status === "active" ? "active" : item.status === "pending" ? "pending" : "blacklist" }`}>{item.status}</div><img className="icon" src={more} alt=" " /></div></td>
+        </tr>
+        ))
+      }
+    </tbody>
+
 
   </table>
-</div>
 
+
+</div> */}
+
+
+<PaginatedItems itemsPerPage={4} />
     </Layout>
   );
 };
+
+
 
 export default User;
