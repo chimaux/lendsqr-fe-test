a6846a9275ad94dbacc282d626f656ce3b58b6b5
Chimaux <ugoppromise@gmail.com>
2023-11-04 02:07:46 +0100

    filter component fix

---
+++ src/screen/user/userMoreItem/UserMoreItems.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import "./UserMoreItems.scss"
+export const UserMoreItems = () => {
+  return (
+    <div className='container'>
+
+    </div>
+  )
+}
+
+export default UserMoreItems
